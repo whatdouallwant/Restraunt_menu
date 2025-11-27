93f38aee862e86d382b3dbcde96075896e3474e7
whatdouallwant <masya.babenko@gmail.com>
2025-11-27 18:38:30 +0200

    added htmls, views, urls, buttons

--- menu_app/views.py
+++ menu_app/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from menu_app.models import Dish
+from menu_app.models import Dish, CartItem
 
 def home(request):
     dishes = Dish.objects.all()
 
     return render(request, "menu/home.html", {
         "dishes": dishes,
+    })
+
+def cart(request):
+    cart_items = CartItem.objects.all()
+
+    return render(request, "menu/cart.html", {
+        "cart_items": cart_items,
+    })
+
+def dish_detail(request, dish_id):
+    dish = Dish.objects.get(id=dish_id)
+
+    return render(request, "menu/dish_info.html", {
+        "dish": dish,
     })
--- menu_project/urls.py
+++ menu_project/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from menu_app.views import home
+from django.urls import include, path
+from menu_app.views import home, cart, dish_detail
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('cart/', cart, name='cart'),
+    path('dish/<int:dish_id>/', dish_detail, name='dish_detail'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
